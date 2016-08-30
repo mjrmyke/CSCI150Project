63b15ef43fe9377b4417c9124bb54b771ca362bd
IIMasterXII <erich.showalter@gmail.com>
2016-08-30 16:02:41 -0700

    now parses the template

--- main.go
+++ main.go
@@ -3,13 +3,23 @@ package main
 import (
 	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"fmt"
+	"text/template"
+	"log"
 )
 
 func init() {
 	r := httprouter.New()
 	http.Handle("/", r)
 	r.GET("/", func(res http.ResponseWriter, req *http.Request, params httprouter.Params){
-		fmt.Fprint(res,"Hello World")
+		tpl, err := template.ParseFiles("index.gohtml")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		err = tpl.Execute(res, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
 	})
 }

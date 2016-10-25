0af62391efd51ee8403ed8dcd0551343c41c3d09
Ken Will <kennethwilleford@gmail.com>
2016-10-25 15:53:59 -0700

    [Integration] Integrated Parser into CORE_Template, Requesting Merge

--- CORE_Template.go
+++ CORE_Template.go
@@ -3,7 +3,10 @@ package main
 import (
 	"html/template"
 	"net/http"
-
+	
+	"github.com/russross/blackfriday"	// russross markdown parser
+	"strings"
+	"regexp"
 	"golang.org/x/net/context"
 	"google.golang.org/appengine"
 )
@@ -50,4 +53,16 @@ func MakeHeader(res http.ResponseWriter, req *http.Request, login, register bool
 	return &HeaderData{
 		login, register, appengine.NewContext(req), u, redirectURL,
 	}
+}
+
+
+/// Parses markdown to produce HTML.
+func parse(inp string) string {
+	inp = strings.Replace(inp,`- [ ]`,`- <input type="checkbox">`,-1)			// Set Unchecked Checkbox
+	inp = strings.Replace(inp,`- [x]`,`- <input type="checkbox" checked>`,-1)	// Set Checked Checkbox
+	data := []byte(inp)															// Convert to Byte
+	regex , _ := regexp.Compile("[sS][cC][rR][iI][pP][tT]")						// Escape Script Tag
+	data = regex.ReplaceAll(data,[]byte("&#115;&#99;&#114;&#105;&#112;&#116;")) 
+	data = blackfriday.MarkdownCommon(data)										// Get Common Markdown from russross's parser
+	return string(data)
 }

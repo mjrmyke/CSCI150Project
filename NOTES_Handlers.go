4a60169f643f58308f1cdbe0dca8a2698809124b
Myke <mjrmyke@gmail.com>
2016-11-09 15:28:18 -0800

    Notes can be created, saved, edited, and viewed

--- NOTES_Handlers.go
+++ NOTES_Handlers.go
@@ -16,6 +16,7 @@ const (
 	PATH_NOTES_New      = "/new"
 	PATH_NOTES_View     = "/view/:ID"
 	PATH_NOTES_Editor   = "/edit/:ID"
+	PATH_NOTES_Edit     = "/edit/"
 	PATH_NOTES_EditRaw  = "/rawedit/:ID"
 )
 
@@ -24,7 +25,7 @@ func INIT_NOTES_HANDLERS(r *httprouter.Router) {
 	r.POST(PATH_NOTES_New, NOTES_POST_New)
 	r.GET(PATH_NOTES_View, NOTES_GET_View)
 	r.GET(PATH_NOTES_Editor, NOTES_GET_Editor)
-	r.POST(PATH_NOTES_Editor, NOTES_POST_Editor)
+	r.POST(PATH_NOTES_Edit, NOTES_POST_Editor)
 	r.GET(PATH_NOTES_EditRaw, NOTES_GET_EditRaw)
 	r.POST(PATH_NOTES_EditRaw, NOTES_GET_EditRaw)
 	r.GET(PATH_NOTES_document, NOTES_GET_DOCUMENT)   // TODO: remove prototype
@@ -114,23 +115,99 @@ func NOTES_GET_View(res http.ResponseWriter, req *http.Request, params httproute
 
 	ServeTemplateWithParams(res, "viewNote", struct {
 		HeaderData
-		ErrorResponse, RedirectURL, Title string
-		Content                           template.HTML
+		ErrorResponse, RedirectURL, Title, Notekey string
+		Content                                    template.HTML
 	}{
 		HeaderData:    *MakeHeader(res, req, false, true),
 		RedirectURL:   req.FormValue("redirect"),
 		ErrorResponse: req.FormValue("ErrorResponse"),
 		Title:         ViewContent.Title,
+		Notekey:       NoteKeyStr,
 		Content:       Body,
 	})
 
 }
 
 /// TODO: implement
-func NOTES_GET_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {}
+func NOTES_GET_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	_, err := GetUserFromSession(req) // Check if a user is already logged in.
+	ctx := appengine.NewContext(req)
+
+	NoteKeyStr := params.ByName("ID")
+	NoteKey, err := strconv.ParseInt(NoteKeyStr, 10, 64)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	ViewNote := &Note{}
+	ViewContent := &Content{}
+
+	err = retrievable.GetEntity(ctx, NoteKey, ViewNote)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	err = retrievable.GetEntity(ctx, ViewNote.ContentID, ViewContent)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	Body := template.HTML(ViewContent.Content)
+	ServeTemplateWithParams(res, "editnote", struct {
+		HeaderData
+		ErrorResponse, RedirectURL, Title, Notekey string
+		Content                                    template.HTML
+	}{
+		HeaderData:    *MakeHeader(res, req, false, true),
+		RedirectURL:   req.FormValue("redirect"),
+		ErrorResponse: req.FormValue("ErrorResponse"),
+		Title:         ViewContent.Title,
+		Notekey:       NoteKeyStr,
+		Content:       Body,
+	})
+}
 
 /// TODO: implement
-func NOTES_POST_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {}
+func NOTES_POST_Editor(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+	_, err := GetUserFromSession(req) // Check if a user is already logged in.
+	//TODO DETERMInE IF THEY HAVE PERMISSION
+	ctx := appengine.NewContext(req)
+
+	if err != nil {
+		http.Redirect(res, req, "/"+req.FormValue("redirect"), http.StatusSeeOther)
+		return
+	}
+
+	data := req.FormValue("note")
+	title := req.FormValue("title")
+	notekey := req.FormValue("notekey")
+
+	Note := &Note{}
+
+	intkey, err := strconv.ParseInt(notekey, 10, 64)
+
+	err = retrievable.GetEntity(ctx, intkey, Note)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	Content := &Content{}
+
+	err = retrievable.GetEntity(ctx, Note.ContentID, Content)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	Content.Content = data
+	Content.Title = title
+
+	_, err = retrievable.PlaceEntity(ctx, Note.ContentID, Content)
+	if ErrorPage(ctx, res, nil, "Internal Server Error (2)", err, http.StatusSeeOther) {
+		return
+	}
+
+	http.Redirect(res, req, "/view/"+notekey, http.StatusSeeOther)
+}
 
 /// TODO: implement
 func NOTES_GET_EditRaw(res http.ResponseWriter, req *http.Request, params httprouter.Params) {}
